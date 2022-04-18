0bcd55189e22083180f775002d2f0ccbcd1212fa
biplob4 <biplobhossen6020@gmail.com>
2022-04-18 15:44:44 +0600

    add blogs

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -4,7 +4,18 @@ import './Blogs.css'
 const Blogs = () => {
     return (
         <div>
-            blogs
+            <div className="blogs w-50 text-center container p-3">
+                <h3 className='text-primary mt-3'>Difference between authorization and authentication?</h3>
+                <p className='text-secondary'></p>
+            </div>
+            <div className="blogs w-50 text-center p-3">
+                <h3 className='text-primary mt-3'>Why are you using firebase? What other options do you have to implement authentication?</h3>
+                <p className='text-secondary'></p>
+            </div>
+            <div className="blogs w-50 text-center p-3">
+                <h3 className='text-primary mt-3'>What other services does firebase provide other than authentication?</h3>
+                <p className='text-secondary'></p>
+            </div>
         </div>
     );
 };
